6859242294dc7ed4784d7571669824113eca6cf6
Jacob Enders <jacobenders1@gmail.com>
2020-10-12 11:33:02 -0400

    update service worker

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,4 @@
-var currentCache = 'v2.9.1';
+var currentCache = 'v2.9.2';
 
 var states = [];
 var lands = [];
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var indexCache = 'i2.9.1';
-var staticCache = 's2.9.1';
+var indexCache = 'i2.9.2';
+var staticCache = 's2.9.2';
 
 var _indexCache = [
 	'./',
